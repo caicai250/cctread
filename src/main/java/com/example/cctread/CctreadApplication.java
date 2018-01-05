5f2ee0be850507aefd5e5db9e317d3e507f636c6
caic <1551316691@qq.com>
2018-01-05 16:20:26 +0800

    可以

---
+++ src/main/java/com/example/cctread/CctreadApplication.java
@@ -0,0 +1,12 @@
+package com.example.cctread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctreadApplication.class, args);
+	}
+}
