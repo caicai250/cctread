e54829ea08b84b958a1458c4e85ce135a499be9e
caic <1551316691@qq.com>
2018-07-15 15:03:53 +0800

    增加获取小说封面

--- src/main/java/com/example/cctread/controller/NovelController.java
+++ src/main/java/com/example/cctread/controller/NovelController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
 /**
  * @Auther: caic
  * @Date: 2018/7/12 15:01
@@ -34,5 +37,35 @@ public class NovelController {
         String downUrl=novelService.getShowdownUrl("/book/"+cctNovel.getNovelId()+"/"+cctNovel.getNovelTitle()+".txt");
         System.out.println(downUrl);
         return cctNovel.toString();
+    }
+
+    /**
+     * 获取小说封面
+     * @param imgPath
+     * @param response
+     */
+    @RequestMapping ( value = "/getNovelCover" )
+    @ResponseBody
+    public void getNovelCover(@RequestParam(value="imgPath") String imgPath,HttpServletResponse response){
+        InputStream input = null;
+        OutputStream out = null;
+        try {
+            out = response.getOutputStream();
+            input=new FileInputStream(new File(imgPath));
+            byte[] b = new byte[1024];
+            int len = 0;
+            while ((len = input.read(b)) != -1) {
+                out.write(b, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) out.close();
+                if (input != null) input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
